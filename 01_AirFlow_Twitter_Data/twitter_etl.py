f6f2a6bba2ec543fc60cdddd1db49e132438131a
AllanMogley <allanmogley@gmail.com>
2023-10-19 23:54:02 +0300

    Authentication Success

--- 01_AirFlow_Twitter_Data/twitter_etl.py
+++ 01_AirFlow_Twitter_Data/twitter_etl.py
@@ -5,20 +5,21 @@ from datetime import datetime
 import s3fs
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
-access_key = os.getenv("API Key")
-access_secret = os.getenv("API Key Secret")
+
+access_key = os.getenv("API_Key")
+access_secret = os.getenv("API_Key_Secret")
 # consumer = os.getenv("Bearer Token")
-consumer_key = os.getenv("Access Token")
-consumer_secret = os.getenv("Access Token Secret")
+consumer_key = os.getenv("Access_Token")
+consumer_secret = os.getenv("Access_Token_Secret")
 
 # Twitter Authentication
 auth = tweepy.OAuthHandler(access_key, access_secret)
-auth.set_access_token(c
-                      , consumer_secret)
+auth.set_access_token(consumer_key, consumer_secret)
 
 # Create an API object
-api - tweepy.API(auth)
+api = tweepy.API(auth)
 
 tweets = api.user_timeline(screen_name="@elonmusk", 
                            count=20, 
